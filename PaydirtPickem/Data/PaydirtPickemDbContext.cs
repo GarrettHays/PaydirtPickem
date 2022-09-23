0d214cb6461276fb6752b07f12c1b58a90f5a18f
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-23 14:17:24 -0700

    recreate context and migrations for db

---
+++ PaydirtPickem/Data/PaydirtPickemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PaydirtPickem.Models;
+
+namespace PaydirtPickem.Data
+{
+    public class PaydirtPickemDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public PaydirtPickemDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<UserPick> UserPicks { get; set; }
+        public DbSet<UserSeasonScore> UserSeasonScores { get; set; }
+        public DbSet<UserWeekScore> UserWeekScores { get; set; }
+
+    }
+}
