e7c9f0bd80da7b79e86eedda7a472f439cf8ed5a
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-23 11:51:51 -0700

    Creates initial database migration

--- PaydirtPickem/Data/Migrations/20220923184914_InitialDatabaseCreation.Designer.cs
+++ PaydirtPickem/Data/Migrations/20220923184914_InitialDatabaseCreation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaydirtPickem.Data;
 
@@ -11,9 +12,10 @@ using PaydirtPickem.Data;
 namespace PaydirtPickem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220923184914_InitialDatabaseCreation")]
+    partial class InitialDatabaseCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PaydirtPickem/Data/Migrations/20220923184914_InitialDatabaseCreation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaydirtPickem.Data.Migrations
+{
+    public partial class InitialDatabaseCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WeekNumber = table.Column<int>(type: "int", nullable: false),
+                    HomeTeam = table.Column<int>(type: "int", nullable: false),
+                    AwayTeam = table.Column<int>(type: "int", nullable: false),
+                    HomeTeamSpread = table.Column<double>(type: "float", nullable: true),
+                    GameTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPicks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    PickedTeam = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GameId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPicks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSeasonScores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SeasonTotalWin = table.Column<int>(type: "int", nullable: false),
+                    SeasonTotalLoss = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSeasonScores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserWeekScores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WeekNumber = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    WeekTotalWin = table.Column<int>(type: "int", nullable: false),
+                    WeekTotalLoss = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWeekScores", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants",
+                column: "ConsumedTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "UserPicks");
+
+            migrationBuilder.DropTable(
+                name: "UserSeasonScores");
+
+            migrationBuilder.DropTable(
+                name: "UserWeekScores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants");
+        }
+    }
+}
