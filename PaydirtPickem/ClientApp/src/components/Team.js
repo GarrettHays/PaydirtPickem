70422b8be80d0311a3e27914df9f37fe8c515fa7
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-30 11:21:14 -0700

    updates to team creation form

--- PaydirtPickem/ClientApp/src/components/Team.js
+++ PaydirtPickem/ClientApp/src/components/Team.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react';
 
 export class Team extends Component {
-    static displayName = Team.name;
+
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            team: '',
+            league: '',
+
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+
+        const data = { name: this.state.name, team: this.state.team, league: this.state.league }
+
+        fetch('/api/createAccount', {
+            method: 'POST',
+
+            body: JSON.stringify(data), // data can be `string` or {object}!
+
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+            .then(res => res.json())
+
+            .catch(error => console.error('Error:', error))
+
+            .then(response => console.log('Success:', response));
+    }
+
+
 
     render() {
         return (
-            <div>
-                <form>
-                    <input
-                        type='text'
-                        name='name'
-                        placeholder='Name' /> <br />
-                    <input
-                        type='text'
-                        name='team'
-                        placeholder='Team Name' /> <br />
-                    <input
-                        type='text'
-                        name='league'
-                        placeholder='Select League to Join' /> <br />
-                    <button type='submit'>submit</button>
-                </form>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="name">Enter Name: </label>
+                <input id="name" name="name" type="text" onChange={this.handleChange} />
+                <br />
+                <label htmlFor="name">Enter Team Name: </label>
+                <input id="name" name="name" type="text" onChange={this.handleChange} />
+                <br />
+                <label htmlFor="league">Select League to Join: </label>
+                <select class="league" id="league" name="league" type="league" onChange={this.handleChange}>
+                    <option value="selected" selected="selected">Available Leagues</option>
+                    <option value="option1" value="option3">League 1</option>
+                    <option value="option2" value="option4">League 2</option>
+                    <option value="option5" value="option6">League 3</option>
+                </select>  
+                <br />
+                <button>Create Team!</button>
+            </form>
         );
     }
 }
