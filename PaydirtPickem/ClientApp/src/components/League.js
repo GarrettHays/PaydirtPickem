79cc98cd25de698fd082adcaae4605dc268d6081
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-09-30 10:48:56 -0700

    refine forms for League Creation

--- PaydirtPickem/ClientApp/src/components/League.js
+++ PaydirtPickem/ClientApp/src/components/League.js
@@ -1,23 +1,85 @@
 import React, { Component } from 'react';
 
 export class League extends Component {
-    static displayName = League.name;
+
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            league: '',
+
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+
+        const data = { name: this.state.name, league: this.state.league }
+
+        fetch('/api/createAccount', {
+            method: 'POST',
+
+            body: JSON.stringify(data), // data can be `string` or {object}!
+
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+            .then(res => res.json())
+
+            .catch(error => console.error('Error:', error))
+
+            .then(response => console.log('Success:', response));
+    }
+
+
 
     render() {
         return (
-            <div>
-                <form>
-                    <input
-                        type='text'
-                        name='name'
-                        placeholder='Name' /> <br />
-                    <input
-                        type='text'
-                        name='league'
-                        placeholder='League Name' /> <br />
-                    <button type='submit'>submit</button>
-                </form>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="name">Enter Name: </label>
+                <input id="name" name="name" type="text" onChange={this.handleChange} />
+                <br/>
+                <label htmlFor="league">Enter League Name: </label>
+                <input id="league" name="league" type="league" onChange={this.handleChange} />
+                <br/>
+                <button>Create League!</button>
+            </form>
         );
     }
 }
+
+
+
+
+//import React, { Component } from 'react';
+
+//export class League extends Component {
+//    static displayName = League.name;
+
+//    render() {
+//        return (
+//            <div>
+//                <form>
+//                    <input
+//                        type='text'
+//                        name='name'
+//                        placeholder='Name' /> <br />
+//                    <input
+//                        type='text'
+//                        name='league'
+//                        placeholder='League Name' /> <br />
+//                    <button type='submit'>submit</button>
+//                </form>
+//            </div>
+//        );
+//    }
+//}
