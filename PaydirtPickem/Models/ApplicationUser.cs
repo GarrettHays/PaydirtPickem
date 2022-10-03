65333353f1846576608790aa7a8c5d2e0e5dad6a
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-10-03 15:47:43 -0700

    adds league models and updates applicationuser model

--- PaydirtPickem/Models/ApplicationUser.cs
+++ PaydirtPickem/Models/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace PaydirtPickem.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public virtual List<League> Leagues { get; set; }   
+        public string TeamName { get; set; }
+
     }
 }
---
+++ PaydirtPickem/Models/League.cs
@@ -0,0 +1,10 @@
+﻿namespace PaydirtPickem.Models
+{
+    public class League
+    {
+        public Guid Id { get; set; }
+        public string LeagueName { get; set; }
+
+        public virtual List<ApplicationUser> Users { get; set; }
+    }
+}
