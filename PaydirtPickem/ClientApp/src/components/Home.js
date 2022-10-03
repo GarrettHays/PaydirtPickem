3077870c25cbbf309920f95380c7d247f73037fa
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-10-03 15:46:34 -0700

    updates home component and css

--- PaydirtPickem/ClientApp/src/components/Home.js
+++ PaydirtPickem/ClientApp/src/components/Home.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
-      <div>
-        <h1>Welcome to Paydirt Pick'em!</h1>
-        <p>Gather your friends, create a league, pick against the spread and get that bag.</p>
-        <p>To get started, head over to the <strong>create league</strong> page.</p>
+       <div className='homeDIV'>
+            <img className='homeIMG' src="https://raw.githubusercontent.com/GarrettHays/images/main/PaydirtWelcome.png" alt="logo"></img>
+            <p>To get started, head over to <strong>register</strong> or <strong>login</strong> page.</p>
+            <p>Then, navigate to <strong>picks</strong> to make your selections!</p>
+            <p>If you've got questions, the <strong>rules</strong> has answers.</p>
+            <br/>
  
-        <p>New Features:</p>
-        <ul>
-          <li><strong>League Creation</strong>: Users can now create private leagues.</li>
-          <li><strong>Individual Team Creation</strong>: Users can now create their own Pick'em teams to participate in Leagues.</li>
-        </ul>
-      </div>
+        {/*<p>new features:</p>*/}
+        {/*<ul>*/}
+        {/*  <li><strong>league creation</strong>: users can now create private leagues.</li>*/}
+        {/*  <li><strong>individual team creation</strong>: users can now create their own pick'em teams to participate in leagues.</li>*/}
+        {/*</ul>*/}
+        </div>
     );
   }
 }
