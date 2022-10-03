79f18f4178430c937e2da70b74bb5fb9242c2a5c
Garrett Hays <103149986+GarrettHays@users.noreply.github.com>
2022-10-03 15:46:53 -0700

    updates NavMenu component and css

--- PaydirtPickem/ClientApp/src/components/NavMenu.js
+++ PaydirtPickem/ClientApp/src/components/NavMenu.js
@@ -26,15 +26,15 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                <NavbarBrand tag={Link} to="/"><img src="https://raw.githubusercontent.com/GarrettHays/images/main/PAYDIRT.png" alt="logo"></img></NavbarBrand>
+                <NavbarBrand tag={Link} to="/"><img src="https://raw.githubusercontent.com/GarrettHays/images/main/PaydirtHEADER.png" alt="logo"></img></NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
                 <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/league">Create League</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/league">League</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/team">Create Team</NavLink>
+                    <NavLink tag={Link} className="text-dark" to="/team">Team</NavLink>
                 </NavItem>
                 <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/leaderboard">Leaderboard</NavLink>
